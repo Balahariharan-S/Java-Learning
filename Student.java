38e6a9d34fd245d605d5b51e22875f1e03468785
Balahariharan-S <balahariharan2004@gmail.com>
2025-08-06 17:31:08 +0530

    My first commit

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student{
+	public static void main(String[]args){
+		String name = "John";
+		String phoneno = "8751265734";
+		int marks = 401;
+		float per = 80.2f;
+		char grade = 'B';
+		System.out.println("Name: "+name);
+		System.out.println("Phone number: "+phoneno);
+		System.out.println("Mark: "+marks);
+		System.out.println("Percentage: "+per+"%");
+		System.out.println("Grade: "+grade);
+	}
+}
